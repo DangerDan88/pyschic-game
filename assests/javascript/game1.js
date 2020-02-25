d45fb00feeb5467f9f09a220654b5964e96ff05b
Danger <51889207+DangerDan88@users.noreply.github.com>
2020-02-25 16:04:29 -0700

    starting to transfer over tp es6 and want to change styling to tailwind css

--- assests/javascript/game1.js
+++ assests/javascript/game1.js
@@ -1,67 +1,90 @@
 alert("you have 9 guesses to win or lose");
 
-var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let computerChoices = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
 
-var wins = 0;
-var losses = 0;
-var guessesLeft = 9;
-var lettersGuessed = [];
-var computerGuess = [];
-
+let wins = 0;
+let losses = 0;
+let guessesLeft = 9;
+let lettersGuessed = [];
+let computerGuess = [];
 
 window.onload = function() {
-var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-  computerGuess.push(compGuess) 
+  let compGuess =
+    computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  computerGuess.push(compGuess);
   console.log(computerGuess[0]);
-
-}
-
+};
 
 document.onkeyup = function(event) {
-    var userGuess = event.key
-    lettersGuessed.push(userGuess);
-    console.log(computerGuess[0]);
-
-
-    if(userGuess === computerGuess[0] && guessesLeft > 0) {
-        wins++;
-        computerGuess = [];
-        var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-  computerGuess.push(compGuess) 
-  console.log(computerGuess,lettersGuessed,compGuess);
-  guessesLeft = 9;
-      alert("You win");
-
-
-    }
-    else if(userGuess !== computerGuess[0] && guessesLeft > 0) {
-        guessesLeft = guessesLeft-1;
-        var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-
-    }
-
-    else {
-      losses++;
-        guessesLeft = 9;
-        lettersGuessed.length = 0;
-        computerGuess.length = 0;
-        var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-  computerGuess.push(compGuess) 
+  let userGuess = event.key;
+  lettersGuessed.push(userGuess);
   console.log(computerGuess[0]);
-      alert("You lose try again");
 
-    }
-    var html = "<p>Guess what letter I'm thinking of!</p>" +
-    "<p>Wins: " + wins + "</p>" +
-    "<p>Losses: " + losses + "</p>" +
-    "<p>Guesses left: " + guessesLeft + "</p>" +
-    "<p>Your guesses so far: " + lettersGuessed + "</p>";
+  if (userGuess === computerGuess[0] && guessesLeft > 0) {
+    wins++;
+    computerGuess = [];
+    let compGuess =
+      computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    computerGuess.push(compGuess);
+    console.log(computerGuess, lettersGuessed, compGuess);
+    guessesLeft = 9;
+    alert("You win");
+  } else if (userGuess !== computerGuess[0] && guessesLeft > 0) {
+    guessesLeft = guessesLeft - 1;
+    let compGuess =
+      computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  } else {
+    losses++;
+    guessesLeft = 9;
+    lettersGuessed.length = 0;
+    computerGuess.length = 0;
+    let compGuess =
+      computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    computerGuess.push(compGuess);
+    console.log(computerGuess[0]);
+    alert("You lose try again");
+  }
+  let html =
+    "<p>Guess what letter I'm thinking of!</p>" +
+    "<p>Wins: " +
+    wins +
+    "</p>" +
+    "<p>Losses: " +
+    losses +
+    "</p>" +
+    "<p>Guesses left: " +
+    guessesLeft +
+    "</p>" +
+    "<p>Your guesses so far: " +
+    lettersGuessed +
+    "</p>";
 
-document.querySelector("#game").innerHTML = html;
-
-}
-
-
-
-
+  document.querySelector("#game").innerHTML = html;
+};
